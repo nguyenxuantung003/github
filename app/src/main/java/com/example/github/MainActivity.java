a0475ed59f59aa5d8067bd508f38c8948768206a
Sangbigdik <nicholeburrowshj32@gmail.com>
2024-03-14 17:37:01 +0700

    commit sang

--- app/src/main/java/com/example/github/MainActivity.java
+++ app/src/main/java/com/example/github/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // ditme sang tron
         // ditme sang tron lan 2
+        // tung dep trai
     }
 }
